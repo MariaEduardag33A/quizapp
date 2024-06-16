6772c4d266e4e3ba942afe83cbe7035c6d941a5f
Maria Eduarda <mariaeduardagomesdasilva2004@gmail.com>
2024-06-16 03:51:06 +0000

    adicionada a função de jogar novamente e FINALIZAÇÃO DO PROJETO

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -3,10 +3,13 @@ import { verificarTema, trocarTema } from "../../helpers/tema-helper.js";
 const botaoTema = document.querySelector(".tema button");
 const body = document.querySelector("body");
 const assunto = localStorage.getItem("assunto");
+const botaoJogarNovamente = document.querySelector("main button");
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema);
 });
+
+botaoJogarNovamente.addEventListener("click", jogarNovamente);
 
 verificarTema(body, botaoTema);
 
@@ -21,20 +24,27 @@ async function alterarAssunto() {
     assuntoTitulo.innerText = assunto;
 }
 
-alterarAssunto()
+alterarAssunto();
 
 function inserirResultado() {
     const sectionPontuacao = document.querySelector(".pontuacao");
     const divAssunto = document.querySelector(".assunto");
-    const pontos = localStorage.getItem("pontos")
+    const pontos = localStorage.getItem("pontos");
 
     sectionPontuacao.innerHTML = `
         ${divAssunto.outerHTML}
 
         <strong>${pontos}</strong>
 
-            <p>de 10</p>
-    `
+        <p>de 10</p>
+    `;
 }
 
-inserirResultado()
+function jogarNovamente(){
+    localStorage.removeItem("pontos")
+    localStorage.removeItem("assunto")
+
+    window.location.href = "../../index.html"
+}
+
+inserirResultado();
